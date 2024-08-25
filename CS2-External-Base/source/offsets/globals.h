e49ef7e06cbf2ee9ec37c1a2e2e70173f933f716
TempleTerry[x64] <123938029+PhilipPanda@users.noreply.github.com>
2024-08-25 15:45:01 +0200

    Offsets Updated

--- CS2-External-Base/source/offsets/globals.h
+++ CS2-External-Base/source/offsets/globals.h
@@ -7,25 +7,25 @@
 
 namespace offsets {
     // Game offsets
-    constexpr std::ptrdiff_t dwLocalPlayerPawn = 0x17D3810;
-    constexpr std::ptrdiff_t dwEntityList = 0x1968518;
-    constexpr std::ptrdiff_t dwViewMatrix = 0x19CA4A0;
-    constexpr std::ptrdiff_t dwViewAngles = 0x19D98F8;
+    constexpr std::ptrdiff_t dwLocalPlayerPawn = 0x17D37F0;
+    constexpr std::ptrdiff_t dwEntityList = 0x19684F8;
+    constexpr std::ptrdiff_t dwViewMatrix = 0x19CA480;
+    constexpr std::ptrdiff_t dwViewAngles = 0x19D98E8;
 
     // Player details
     constexpr std::ptrdiff_t m_pCameraServices = 0x1130;
     constexpr std::ptrdiff_t m_iFOV = 0x210;
     constexpr std::ptrdiff_t m_bIsScoped = 0x22A0;
-    constexpr std::ptrdiff_t attack = 0x17CC040;
+    constexpr std::ptrdiff_t attack = 0x17CC020;
     constexpr std::ptrdiff_t m_iIDEntIndex = 0x13A8;
-    constexpr std::ptrdiff_t flFlashDuration = 0x1468;
+    constexpr std::ptrdiff_t flFlashDuration = 0x135C;
 
     // Entity details
     constexpr std::ptrdiff_t m_hPawn = 0x60C;
     constexpr std::ptrdiff_t m_iHealth = 0x324;
     constexpr std::ptrdiff_t m_iTeamNum = 0x3C3;
     constexpr std::ptrdiff_t m_vOldOrigin = 0x1274;
-    constexpr std::ptrdiff_t m_entitySpottedState = 0x10F8;
+    constexpr std::ptrdiff_t m_entitySpottedState = 0x2288;
     constexpr std::ptrdiff_t m_vecViewOffset = 0xC50;
 }
 
