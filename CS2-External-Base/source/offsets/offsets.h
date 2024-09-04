073a37acbbbfaa729fff97b8bc16ecc6d26974b8
TempleTerry[x64] <123938029+PhilipPanda@users.noreply.github.com>
2024-09-04 14:06:14 +0200

    Update offsets.h

--- CS2-External-Base/source/offsets/offsets.h
+++ CS2-External-Base/source/offsets/offsets.h
@@ -4,17 +4,17 @@
 
 namespace offsets {
     // Game offsets
-    constexpr std::ptrdiff_t dwLocalPlayerPawn = 0x17D37F0;
-    constexpr std::ptrdiff_t dwEntityList = 0x19684F8;
-    constexpr std::ptrdiff_t dwViewMatrix = 0x19CA480;
-    constexpr std::ptrdiff_t dwViewAngles = 0x19D98E8;
+    constexpr std::ptrdiff_t dwLocalPlayerPawn = 0x17C37F0;
+    constexpr std::ptrdiff_t dwEntityList = 0x1956A68;
+    constexpr std::ptrdiff_t dwViewMatrix = 0x19B89F0;
+    constexpr std::ptrdiff_t dwViewAngles = 0x19C7E58;
     constexpr std::ptrdiff_t m_vecOrigin = 0x548;
 
     // Player details
     constexpr std::ptrdiff_t m_pCameraServices = 0x1130;
     constexpr std::ptrdiff_t m_iFOV = 0x210;
     constexpr std::ptrdiff_t m_bIsScoped = 0x22A0;
-    constexpr std::ptrdiff_t attack = 0x17CC020;
+    constexpr std::ptrdiff_t attack = 0x17BC020;
     constexpr std::ptrdiff_t m_iIDEntIndex = 0x13A8;
     constexpr std::ptrdiff_t flFlashDuration = 0x135C;
 
@@ -25,4 +25,5 @@ namespace offsets {
     constexpr std::ptrdiff_t m_vOldOrigin = 0x1274;
     constexpr std::ptrdiff_t m_entitySpottedState = 0x2288;
     constexpr std::ptrdiff_t m_vecViewOffset = 0xC50;
+    constexpr std::ptrdiff_t m_iObserverMode = 0x40;
 }
